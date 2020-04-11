a5dff8a947856a9981d7d479e55e8108a20c6bd2
Luke Hammer <textchimp@gmail.com>
2020-04-11 22:30:38 +1000

    initial commit of starter code

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Farmyard Fight Club
+### A three.js 3D Coronavirus Codealong
+
+___
+
+
+
+
+
+
+
+CREDITS
+-------
+
+3D Character Models: https://opengameart.org/users/quaternius
+
+3D Scenery Models: https://www.cgtrader.com/free-3d-models/plant/other/low-poly-trees-d9e99730-93d6-4564-a477-7ec52a990a3c
+
+Grass texture: https://github.com/mxro/threejs-test/tree/master/test4/public/textures
+
+Inspiring tutorial: https://threejsfundamentals.org/threejs/lessons/threejs-game.html
+
+Backgrounds & skyboxes: https://threejsfundamentals.org/threejs/lessons/threejs-backgrounds.html
---
+++ js/scenery.js
@@ -0,0 +1,58 @@
+
+app.scenery = {};
+
+const OBJFile = 'models/scenery/trees.obj';
+const MTLFile = 'models/scenery/trees.mtl';
+const PNGFilePrefix = 'models/scenery/';
+
+app.sceneryCount = 20;
+
+app.addScenery = name => {
+  if( !(name in app.scenery.models) ) {
+    return console.error('Scenery model not defined:', name);
+  }
+  const itemClone = app.scenery.models[name].clone();
+  const spread = 300;
+  itemClone.position.set(
+    THREE.MathUtils.randFloatSpread(spread),
+    0, // THREE.MathUtils.randFloatSpread(spread),
+    THREE.MathUtils.randFloatSpread(spread)
+  );
+  itemClone.scale.set(10,10,10);
+  app.scene.add(itemClone);
+};
+
+
+const sceneryManager = new THREE.LoadingManager();
+sceneryManager.onLoad = () => {
+  const names = Object.keys(app.scenery.models);
+  for( let i = 0; i < app.sceneryCount; i++ ){
+    app.addScenery( app.lib.randArray(names) );
+  }
+};
+
+var textureLoader = new THREE.TextureLoader();
+
+new THREE.MTLLoader()
+  .load(MTLFile, function (materials) {
+    materials.preload();
+    new THREE.OBJLoader(sceneryManager)
+      .setMaterials(materials)
+      .load(OBJFile, function (object) {
+        object.position.set(0,0,0);
+        app.scenery = object;
+        app.scenery.models = {}; // naughty
+        object.traverse( o => {
+          if( o.isMesh ){
+            const name = o.material.name;
+            // Now have to manually load each texture for each model and manually
+            // attach it to the material and it's so fucking barbaric
+            const texture = textureLoader.load(`${PNGFilePrefix}${name}.png`);
+            o.position.set(0,0,0);
+            o.material.map = texture;
+            o.material.shininess = 0; // !!!!
+            app.scenery.models[ o.material.name ] = o; // add to lookup object:
+          }
+        });
+      }); // load OBJ
+  }); // load MTL
